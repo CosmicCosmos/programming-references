fa8e8955d91b23f9582fb2e4729acca29aa0c32f
syarram <syarram@lendingclub.com>
2017-12-14 22:48:44 -0800

    CR-2 Reverse a String Optimal Solution

--- src/main/java/net/learnings/programming/string/ReverseAString.java
+++ src/main/java/net/learnings/programming/string/ReverseAString.java
@@ -1,4 +1,23 @@
 package net.learnings.programming.string;
 
+import org.testng.annotations.Test;
+
 public class ReverseAString {
+
+    String actual ="reverse";
+
+    char temp;
+
+    @Test
+    public void reverseBySwap(){
+       char[] before = actual.toCharArray();
+       int j = actual.length()-1;
+       for(int i=0;i<actual.length()/2;i++,j--){
+           temp = before[i];
+           before[i] = before[j];
+           before[j] = temp;
+        }
+        System.out.println(String.valueOf(before));
+    }
+
 }
